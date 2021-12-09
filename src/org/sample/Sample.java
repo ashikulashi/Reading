a6dcfbca374b5fea177c337cd1b7e6be0f5360cd
balabv321 <balabv321@gmail.com>
2021-12-09 16:45:38 +0530

    Asar is going do work

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,11 @@ package org.sample;
 
 public class Sample {
 	
+	private void code2() {
+		System.out.println("Code2");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Codes");
