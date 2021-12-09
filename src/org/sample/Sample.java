f6dbdc5b2ebdf3463e26d50980d3814c762aae98
balabv321 <balabv321@gmail.com>
2021-12-09 16:08:43 +0530

    Ashik has written a codes

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,10 @@ package org.sample;
 
 public class Sample {
 	
+	private void codes1() {
+		System.out.println("codes 1");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Codes");
